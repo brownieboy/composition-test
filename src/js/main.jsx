c7e5bb7979b03d336efb75b9cc734eb6c7a759b2
Michael Brown <brownieboy@gmail.com>
2015-10-24 13:38:34 +1100

    First commit

---
+++ src/js/main.jsx
@@ -0,0 +1,43 @@
+var Vehicle = React.createClass({
+    getVehicleType: function() {
+        return this.props.getVehicleType ? this.props.getVehicleType() : "Vehicle";
+    },
+
+    handleClick: function(e) {
+        this.props.handleClick ? this.props.handleClick() : null;
+        console.log("Engine started");
+    },
+
+    render: function() {
+        return (
+            <div>
+                <h1>I'm a {this.getVehicleType()}</h1>
+                <button onClick={this.handleClick}>Start</button>
+            </div>
+        );
+    }
+});
+
+var Airplane = React.createClass({
+    getVehicleType: function() {
+        return this.props.getVehicleType ? this.props.getVehicleType() : "Airplane";
+    },
+
+    handleClick: function(e) {
+        this.props.handleClick ? this.props.handleClick() : null;
+        console.log("Flying!");
+    },
+
+    render: function() {
+        return (
+            <Vehicle
+                getVehicleType = {this.getVehicleType}
+                handleClick = {this.handleClick} />
+        );
+    }
+
+});
+
+ReactDOM.render(<Airplane  />, document.getElementById('airplane'));
+ReactDOM.render(<Vehicle  />, document.getElementById('vehicle'));
+
